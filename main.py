e125d1be2823c39b733d84720353973870fcee47
LDTips <batruchs@gmail.com>
2023-10-31 17:16:25 +0100

    Moved file reading to another script + emptyline additions before function definitions

--- main.py
+++ main.py
@@ -1,84 +1,7 @@
 import copy
 import random
 import math
-
-
-def process_demands(f_arr, index):
-    # demand_volue - h_d
-    demands = []
-    f_arr = f_arr[index+1:]  # f_arr[index] is the definition of the section
-    arr_iter = iter(f_arr)  # Easier to do with iterator rather than classic for loop
-
-    while True:
-        try:
-            current = next(arr_iter).split(' ')
-            if '#' in current[0] or current[0] == '':  # Check if another section is not present
-                break
-        except StopIteration:  # Raised if iterator is exhausted
-            break
-
-        # Reads info from first line which is in format: demand_id   link_nodeA   link_nodeB   demand_volume
-        link = (current[1], current[2])
-        h_d = current[3]
-
-        path_no = int(next(arr_iter))  # Amount of paths is defined after the first line
-
-        paths = []
-        for _ in range(path_no):
-            current_path = next(arr_iter).strip().split(' ')
-            nodes = {int(i) for i in current_path[1:]}  # Read vertices into a set. First digit is demand_id, we skip it
-            paths.append(nodes)
-
-        next(arr_iter)  # Skip the empty line that separates each link & demands
-
-        demands.append(
-            {
-                "demand": link,
-                "paths": paths,
-                "h_d": int(h_d)
-            }
-        )
-
-    return demands
-
-
-def process_links(f_arr, index):
-    links = []
-    f_arr = f_arr[index+1:]  # f_arr[index] is the definition of the section
-    arr_iter = iter(f_arr)  # Easier to do with iterator rather than classic for loop
-
-    while True:
-        current = next(arr_iter).split(' ')
-        if current[0] == '':
-            break
-        links.append(
-            {
-                'link': (current[1], current[2]),
-                'module_count': current[3],
-                'module_cost': current[4]
-            }
-        )
-
-    return links
-
-
-def read_file(filepath):
-    with open(filepath, 'r') as file:
-        f_arr = file.read().splitlines()  # Removes newline characters compared to readlines()
-
-    non_complex = {'module_capacity': 0, 'number_of_links': 0, 'number_of_demands': 0}  # These do not require parsing
-    demands, links = [], []
-    for index, line in enumerate(f_arr):
-        section = line.split('   ')[0]
-        if any('#' + i == section for i in non_complex.keys()):
-            # Variable names in file are the same as in the non_complex dict. slice [1:] is used for strip the #
-            non_complex[line[1:]] = int(f_arr[index+1])  # index+1 fetches the value from the next line
-        elif section == "#number_of_links_in_the path":
-            demands = process_demands(f_arr, index)
-        elif section == "#link_id":
-            links = process_links(f_arr, index)
-
-    return non_complex, demands, links
+from read_file import read_file
 
 
 def random_sum(n, total):
@@ -125,6 +48,7 @@ def calculate_link_overloads(loads, links, module_cap):
 
     return overloads
 
+
 def crossover(parent1, parent2, crossover_probability):
     offspring1 = {str(i): [] for i in range(1, len(parent1) + 1)}
     offspring2 = {str(i): [] for i in range(1, len(parent1) + 1)}
@@ -151,11 +75,13 @@ def mutate(chromosomes, mutation_probability):
                 chromosome[i][decreased_index] -= 1
                 chromosome[i][increased_index] += 1
 
+
 def generate_start_population(population_size, demands):
     start_population = []
     while len(start_population) < population_size:
         start_population.append(random_chromosome(demands))
     return start_population
+
 
 def find_best_objective_function(chromosomes, demands, links, module_capacity):
     best_objective_function = float('inf')
@@ -263,6 +189,7 @@ def main():
     print("Simulation finished")
     print("Best solution:", best_solution)
     print("Objective function:", min_f)
+
 
 if __name__ == "__main__":
     main()
---
+++ read_file.py
@@ -0,0 +1,80 @@
+def process_demands(f_arr, index):
+    # demand_volue - h_d
+    demands = []
+    f_arr = f_arr[index+1:]  # f_arr[index] is the definition of the section
+    arr_iter = iter(f_arr)  # Easier to do with iterator rather than classic for loop
+
+    while True:
+        try:
+            current = next(arr_iter).split(' ')
+            if '#' in current[0] or current[0] == '':  # Check if another section is not present
+                break
+        except StopIteration:  # Raised if iterator is exhausted
+            break
+
+        # Reads info from first line which is in format: demand_id   link_nodeA   link_nodeB   demand_volume
+        link = (current[1], current[2])
+        h_d = current[3]
+
+        path_no = int(next(arr_iter))  # Amount of paths is defined after the first line
+
+        paths = []
+        for _ in range(path_no):
+            current_path = next(arr_iter).strip().split(' ')
+            nodes = {int(i) for i in current_path[1:]}  # Read vertices into a set. First digit is demand_id, we skip it
+            paths.append(nodes)
+
+        next(arr_iter)  # Skip the empty line that separates each link & demands
+
+        demands.append(
+            {
+                "demand": link,
+                "paths": paths,
+                "h_d": int(h_d)
+            }
+        )
+
+    return demands
+
+
+def process_links(f_arr, index):
+    links = []
+    f_arr = f_arr[index+1:]  # f_arr[index] is the definition of the section
+    arr_iter = iter(f_arr)  # Easier to do with iterator rather than classic for loop
+
+    while True:
+        current = next(arr_iter).split(' ')
+        if current[0] == '':
+            break
+        links.append(
+            {
+                'link': (current[1], current[2]),
+                'module_count': current[3],
+                'module_cost': current[4]
+            }
+        )
+
+    return links
+
+
+def read_file(filepath):
+    with open(filepath, 'r') as file:
+        f_arr = file.read().splitlines()  # Removes newline characters compared to readlines()
+
+    non_complex = {'module_capacity': 0, 'number_of_links': 0, 'number_of_demands': 0}  # These do not require parsing
+    demands, links = [], []
+    for index, line in enumerate(f_arr):
+        section = line.split('   ')[0]
+        if any('#' + i == section for i in non_complex.keys()):
+            # Variable names in file are the same as in the non_complex dict. slice [1:] is used for strip the #
+            non_complex[line[1:]] = int(f_arr[index+1])  # index+1 fetches the value from the next line
+        elif section == "#number_of_links_in_the path":
+            demands = process_demands(f_arr, index)
+        elif section == "#link_id":
+            links = process_links(f_arr, index)
+
+    return non_complex, demands, links
+
+
+if __name__ == "__main__":
+    pass
